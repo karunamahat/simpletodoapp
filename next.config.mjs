bcac0c7bae30ad5fc26020dbc4bb99fb40694b12
Karuna <mahatkaruna1994@gmail.com>
2024-06-11 13:43:35 +0545

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/simpletodo",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
+git;
